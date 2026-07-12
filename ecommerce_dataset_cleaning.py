89d84e483c4dc648982c6b349d1bd57b69f09961
Divyansh0410-svg <divyanshpersonal2008@gmail.com>
2026-07-12 13:57:54 +0530

    Dropping rows with missing categories

--- ecommerce_dataset_cleaning.py
+++ ecommerce_dataset_cleaning.py
@@ -28,8 +28,17 @@ ecom_data['order_date']=pd.to_datetime(ecom_data['order_date'], errors='coerce')
 logging.info(f'Converting dtype of order_date column to datetime')
 
 #Converting columns like price and quantity to float and integer respectively 
-# and dropping columns with non numerical value
+#and dropping columns with non numerical value
 ecom_data['quantity']=pd.to_numeric(ecom_data['quantity'], errors='coerce').astype('Int64')
 ecom_data['price']=pd.to_numeric(ecom_data['price'], errors='coerce').astype('Float64')
 logging.info(f'Convertion of price and quantity columns to int and float is successfull')
-ecom_data.info()
+
+#Dropping rows with missing category
+missing_category= ecom_data['category'].isnull().sum()
+ecom_data.dropna(subset=['category'], inplace=True)
+if ecom_data['category'].isnull().sum()==0:
+    logging.info('QUALITY GATE PASSES: No missing value remains')
+else:
+    logging.critical(f'QUALITY GATE FAILED: Found {missing_category} null values')
+    #Physically stopping the script
+    raise ValueError('Pipleline halted due to unhealed missing values')
