2f58e4e880db0db75b0f4db7d71652389dbadd2c
Divyansh0410-svg <divyanshpersonal2008@gmail.com>
2026-07-12 09:48:05 +0530

    Imported libraries and set up logging data

---
+++ ecommerce_dataset_cleaning.py
@@ -0,0 +1,28 @@
+#Importing all the libraries needed
+import pandas as pd
+import numpy as np
+import logging
+
+#Setting up logging data
+logging.basicConfig(
+    filename='ecommerce_sales.log',
+    filemode='w',  # Force it to overwrite fresh every single run
+    level=logging.INFO, 
+    format='%(asctime)s - [%(levelname)s] - %(message)s'
+)
+
+#Reading and loading the raw dataset
+ecom_data=pd.read_csv('messy_ecommerce_sales_data.csv')
+logging.info(f"Succesfully loaded the the file, initial shape is: {ecom_data.shape}")
+
+#Stip the unwanted space of the column headers and turning them in lowercase
+ecom_data.columns=ecom_data.columns.str.strip().str.lower()
+
+#Dropping the column which are not required
+drop_columns=['id','customer_name','order_id']
+ecom_data.drop(drop_columns, axis=1, inplace=True)
+logging.info(f'Tracking dropped columns, Remaining columns are; {list(ecom_data.columns)}')
+
+#Converting order_date format from str to datetime
+ecom_data['order_date']=pd.to_datetime(ecom_data['order_date'], errors='coerce')
+logging.info(f'Converting dtype of order_date column to datetime')
